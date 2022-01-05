91073b995d56c6b6f7460a55c2b087b6f29a6352
Ardaerd <arderd@outlook.com>
2022-01-05 19:06:39 +0300

    Hash table is done

--- Hash_Table/main.cpp
+++ Hash_Table/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include "Hash.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    // array that contains keys to be mapped
+    int a[] = {15,11,27,8,12};
+    int n = sizeof(a) / sizeof(a[0]);
+    cout << sizeof(a[0]) << " " << n << " " << sizeof(a) << endl;
+
+    // insert the keys into the hash table
+    Hash h(7);   // 7 is count of buckets in hash table
+
+    for (int i = 0; i < n; i++)
+        h.insertItem(a[i]);
+
+    // delete 12 from hash table
+    h.deleteItem(12);
+
+    // display the hash table
+    h.displayHash();
+
     return 0;
 }
